7d9f9fd8ec1b44819aae7ba958df3c3c1ee09c17
Kenneth Kang & Sean Kung <pair+kangkg.khjs534@devbootcamp.com>
2016-11-25 16:03:30 -0600

    fixed answers and added answers view to show

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,17 +1,17 @@
-get '/questions/:id/answers/new' do 
+get '/questions/:id/answers/new' do
 	@question_id = params[:id]
 	erb :"/answers/new"
 end
 
 post '/answers' do
-	question_id = parmas[:question_id]
+	question_id = params[:question_id]
 	answer = params[:answer]
 	user_id = current_user.id
-	new_answer = Answer.new({answer: answer,user_id: user_id,question_id: question_id})
+	new_answer = Answer.new({answer: answer,answerer_id: user_id,question_id: question_id})
 	if new_answer.save
 		redirect "/questions/#{params[:question_id]}"
 	else
-		@error = answer.errors.full_messages 
+		@error = answer.errors.full_messages
 		erb :"/answers/new"
 	end
 end
