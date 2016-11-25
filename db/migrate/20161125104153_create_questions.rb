503a6114886e54a8fb77e6138af505407a1190bf
Vince Roti <vince.roti@gmail.com>
2016-11-25 11:27:26 -0600

    add controller folder with user controller

--- db/migrate/20161125104153_create_questions.rb
+++ db/migrate/20161125104153_create_questions.rb
@@ -2,10 +2,10 @@ class CreateQuestions < ActiveRecord::Migration
   def change
   	 create_table :questions do |t|
       t.string :title, { limit: 64, null: false }
-      t.text :body, { null: false }
-      t.integer :author_id { null: false}
+      t.text :body
+      t.integer :author_id, { null: false  }
       t.integer :best_answer_id 
-      
+
       t.timestamps(null: false)
     end
   end
