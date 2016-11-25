960743128d875532add83682d161c2b441772a63
Kenneth Kang & Sean Kung <pair+kangkg.khjs534@devbootcamp.com>
2016-11-25 12:03:47 -0600

    skeleton of questions controller only

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,27 @@
+get '/questions' do
+ # show all questions: index
+end
+
+get '/questions/new' do
+ # new question form
+end
+
+post '/questions' do
+  # new question created
+end
+
+delete '/questions/:id' do
+
+end
+
+get '/questions/show' do
+
+end
+
+get '/questions/show' do
+
+end
+
+get '/questions/show' do
+
+end
